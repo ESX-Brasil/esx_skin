be9633012db8e2d5ba08393d032ee31ac4e97d4a
renildomarcio <krsolucoesweb@gmail.com>
2019-06-16 11:04:27 -0300

    Update V1.1.2 @ESX-Brasil

--- server/main.lua
+++ server/main.lua
@@ -6,9 +6,8 @@ RegisterServerEvent('esx_skin:save')
 AddEventHandler('esx_skin:save', function(skin)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	MySQL.Async.execute('UPDATE users SET `skin` = @skin WHERE identifier = @identifier',
-	{
-		['@skin']       = json.encode(skin),
+	MySQL.Async.execute('UPDATE users SET skin = @skin WHERE identifier = @identifier', {
+		['@skin'] = json.encode(skin),
 		['@identifier'] = xPlayer.identifier
 	})
 end)
@@ -38,15 +37,14 @@ ESX.RegisterServerCallback('esx_skin:getPlayerSkin', function(source, cb)
 	MySQL.Async.fetchAll('SELECT skin FROM users WHERE identifier = @identifier', {
 		['@identifier'] = xPlayer.identifier
 	}, function(users)
-		local user = users[1]
-		local skin = nil
+		local user, skin = users[1]
 
 		local jobSkin = {
 			skin_male   = xPlayer.job.skin_male,
 			skin_female = xPlayer.job.skin_female
 		}
 
-		if user.skin ~= nil then
+		if user.skin then
 			skin = json.decode(user.skin)
 		end
 
@@ -54,7 +52,7 @@ ESX.RegisterServerCallback('esx_skin:getPlayerSkin', function(source, cb)
 	end)
 end)
 
--- Comandos
+-- Commands
 TriggerEvent('es:addGroupCommand', 'skin', 'admin', function(source, args, user)
 	TriggerClientEvent('esx_skin:openSaveableMenu', source)
 end, function(source, args, user)
